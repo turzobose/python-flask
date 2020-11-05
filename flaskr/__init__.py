575e2af8fe5a22c5eb22e5a3da567a5e7cfef94a
turzobose <turzobose@gmail.com>
2020-11-05 23:55:47 +0800

    Added failure.html and presence check on text box

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     number = random.randint(1,100)
-    return render_template("index.html", name ="Turzo", num = number)
+    return render_template("index.html", num=number)
 
 @app.route('/hello')
 def bye():
     name = request.args.get("name")
+    if not name:
+        return render_template("failure.html")
     return render_template("hello.html", name=name)
